734db502b1fd546d3c811d42975ca4ca0777a812
ARNAIM.COM <52743150+masfy@users.noreply.github.com>
2026-01-11 19:58:51 +0800

    Update pdfGenerator.js

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -197,7 +197,7 @@ export function generateInvoicePdf(invoice, storeInfo) {
     doc.setFont('helvetica', 'normal');
     doc.text('Terima kasih atas kunjungan Anda!', pageWidth / 2, yPos, { align: 'center' });
     yPos += lineHeight;
-    doc.text('Minta rela 😊🙏🏼', pageWidth / 2, yPos, { align: 'center' });
+    doc.text('Minta rela.', pageWidth / 2, yPos, { align: 'center' });
     yPos += lineHeight + 2;
     doc.text('Barakallah!', pageWidth / 2, yPos, { align: 'center' });
     yPos += lineHeight + 2;
